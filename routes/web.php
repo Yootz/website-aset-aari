2799700dcf1120e99c06013803f183b9556a4465
Nab <biha.tegar@gmail.com>
2026-09-24 14:26:22 +0700

    Tambah fitur QR Code Create untuk aset

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\DivisionController;
+use App\Http\Controllers\QRCodeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-use App\Http\Controllers\EmployeeController;
-use App\Http\Controllers\DivisionController;
 
 Route::resource('employee', EmployeeController::class);
 Route::resource('division', DivisionController::class);
+
+Route::get('createqr', [QRCodeController::class, 'index'])->name('createqr.index');
